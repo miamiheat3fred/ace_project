c141503f62ba687c4392f1c49b41f2807f36759a
miamiheat3fred <76983029+miamiheat3fred@users.noreply.github.com>
2021-03-14 14:09:01 -0700

    Add files via upload

---
+++ faq/scripts/faq.js
@@ -0,0 +1,53 @@
+/*the lines below are to prevent false errors from jslint or eslint in Brackets*/
+
+/*eslint-disable no-unused-vars*/
+/*global $, document, alert, window*/
+/*jshint browser: true, unused: false, no-unused-vars: true*/
+/*jslint devel: true, node: true, vars: true*/
+"use strict"; // required by jslint
+
+/*
+File Name: faq.js
+Date: 03/03/21
+Programmer: Fredy Chavez
+*/
+
+// The line below will test the link to the dropdown.js file. Comment it out when it works.
+//alert("the link to the .js file is correct!");
+
+// The line below will test the link to jquery.min.js . Comment it out when it works.
+//if (window.jQuery) {alert("the link to the jQuery file is correct!"); }
+
+$(document).ready(function() {
+ 
+    //alert("the jQuery is running!");
+    
+    $("h1").text("Frequently Asked Questions (FAQ)");
+    
+    $("h1").css("color", "#FF0000");
+    
+    //$("h1, h2").addClass("headings");
+    
+    //$(".headings").css("font-family", "Arial, Helvitica, sans-serif");
+    
+    $("dd").hide();
+    
+    $("dt").on("click", function() {
+        
+        //alert("You clicked a question!");
+        
+        //$(this).next().show();
+        
+        //$(this).next().toggle();
+        
+        //$(this).next().slideToggle();
+        
+        //$(this).addClass("open");
+        
+        //$(this).toggleClass("open");
+        
+        $(this).toggleClass("open").next().slideToggle();
+        
+    });
+    
+});
