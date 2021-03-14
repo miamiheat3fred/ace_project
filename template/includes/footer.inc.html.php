700ee7a77d91ac4e1145101169af16f0f661839e
miamiheat3fred <76983029+miamiheat3fred@users.noreply.github.com>
2021-03-14 14:15:13 -0700

    Add files via upload

---
+++ template/includes/footer.inc.html.php
@@ -0,0 +1,26 @@
+<footer>
+            
+            <p>
+            Like Our Social Media Pages!
+            </p>
+        
+            <div class="media">
+            
+                <ul>
+                    <!--Add website URLs in href tags to point to respective social media pages.-->
+                    <li><a href=""><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href=""><i class="fa fa-twitter-square"></i></a></li>
+                </ul>
+        
+            </div>
+            
+            <p id="designer">
+                Images are licensed under the Creative Commons CC0 from <a href="https://pixabay.com">https://pixabay.com</a> . Site design by Fredy Chavez, who has been a part-time student at <a href="http://www.pcc.edu" target="_blank">PCC.</a>
+            </p>
+
+            <hr>
+
+            <p id="contact">Fredy Chavez <a href="http://fredychavez.webhostingforstudents.com/">http://fredychavez.webhostingforstudents.com/</a>
+            </p>
+
+</footer>
---
+++ template/includes/header.inc.html.php
@@ -0,0 +1,23 @@
+<header>
+               
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+            
+    <h1 id="title">Ace in the Hole Multisport Events</h1>
+            
+        <ul class="nav">
+            <li><a href="#" target="_self">Home</a></li>
+            <li><a href="#" target="_self">Events ▾</a>
+                <ul class="dropdown">
+                    <li><a href="#">What to Bring</a></li>
+                    <li><a href="#">Course Details</a></li>
+                    <li><a href="#">FAQs</a></li>
+                </ul>
+            </li>
+            <li><a href="#" target="_self">Register</a></li>
+            <li><a href="#" target="_self">Contact Us</a></li>
+            <li><a href="#" target="_self">Gallery</a></li>
+        </ul>
+               
+</header>
+
